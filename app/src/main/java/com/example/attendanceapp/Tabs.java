81f84839d62c2f9a081e74affefc6704862a62d5
Dhanashree Shetty <33102821+dhanashreesshetty@users.noreply.github.com>
2020-05-31 02:23:21 +0530

    Attributions

--- app/src/main/java/com/example/attendanceapp/Tabs.java
+++ app/src/main/java/com/example/attendanceapp/Tabs.java
@@ -3,9 +3,16 @@ package com.example.attendanceapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Tabs extends AppCompatActivity {
+
+    public void showAttributions(View view){
+        Intent attris=new Intent(this, Attributions.class);
+        startActivity(attris);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
