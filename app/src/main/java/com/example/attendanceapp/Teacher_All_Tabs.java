7bf41c9b05ba369d99b96d7f028e2750e8fc79e6
Dhanashree Shetty <33102821+dhanashreesshetty@users.noreply.github.com>
2020-06-06 14:50:33 +0530

    Teacher tabs

--- app/src/main/java/com/example/attendanceapp/Teacher_All_Tabs.java
+++ app/src/main/java/com/example/attendanceapp/Teacher_All_Tabs.java
@@ -1,6 +1,7 @@
 package com.example.attendanceapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,43 +17,42 @@ import android.view.View;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class Tabs extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class Teacher_All_Tabs extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout drawer;
     FirebaseAuth mAuth;
 
-    public void showAttributions(View view){
-        Intent attris=new Intent(this, Attributions.class);
-        startActivity(attris);
+    public void showAttendance(View view){
+        Intent goToAttendancePage = new Intent(Teacher_All_Tabs.this, TeacherAttendance.class);
+        startActivity(goToAttendancePage);
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tabs);
-        GridLayout mainGrid=(GridLayout)findViewById(R.id.mainGrid);
+        setContentView(R.layout.activity_teacher__all__tabs);
+        GridLayout mainGrid = (GridLayout) findViewById(R.id.mainGrid);
         mainGrid.setTranslationY(10000);
         mainGrid.animate().translationYBy(-10000).setDuration(1000);
 
-        Toolbar toolbar=findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        drawer=findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
 
-        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView=findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
     }
 
     @Override
     public void onBackPressed() {
-        if(drawer.isDrawerOpen(GravityCompat.START)){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         }
     }
@@ -60,13 +60,16 @@ public class Tabs extends AppCompatActivity implements NavigationView.OnNavigati
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         mAuth = FirebaseAuth.getInstance();
-        switch (item.getItemId()){
-            case R.id.nav_announcement: break;
-            case R.id.nav_share: break;
-            case R.id.nav_contact: break;
+        switch (item.getItemId()) {
+            case R.id.nav_announcement:
+                break;
+            case R.id.nav_share:
+                break;
+            case R.id.nav_contact:
+                break;
             case R.id.nav_logout:
                 FirebaseAuth.getInstance().signOut();
-                Intent goBackToLogin = new Intent(Tabs.this, MainActivity.class);
+                Intent goBackToLogin = new Intent(Teacher_All_Tabs.this, TeacherActivity.class);
                 startActivity(goBackToLogin);
         }
         return true;
