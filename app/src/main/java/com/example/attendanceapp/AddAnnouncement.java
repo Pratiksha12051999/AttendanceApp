267143879d93af97c8ecfc96512cb1af18353150
BabliSah <BabliSah@github.com>
2020-06-14 17:00:48 +0530

    add announcement with notification

--- app/src/main/java/com/example/attendanceapp/AddAnnouncement.java
+++ app/src/main/java/com/example/attendanceapp/AddAnnouncement.java
@@ -1,5 +1,8 @@
 package com.example.attendanceapp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,21 +10,21 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.sql.Timestamp;
 
 public class AddAnnouncement extends AppCompatActivity {
-    String Subject;
-    String Content;
-    EditText subject;
-    EditText content;
+    public EditText subject, content;
     Button Add;
     FirebaseDatabase root;
     DatabaseReference mDatabase;
-    String Timestamp;
+    FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +33,48 @@ public class AddAnnouncement extends AppCompatActivity {
         subject = findViewById(R.id.AddSubject);
         content = findViewById(R.id.AddContent);
         root = FirebaseDatabase.getInstance();
-        mDatabase = root.getReference().child("Announcements");
-        Timestamp New = new Timestamp(System.currentTimeMillis());
-        Timestamp =New.toString();
+        mDatabase = root.getReference();
+        final Timestamp New = new Timestamp(System.currentTimeMillis());
+
 
 
         Add = findViewById(R.id.AddAnnouncement);
         Add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Subject = subject.toString();
-                Content = content.toString();
-                Ann ann = new Ann(Subject, Content, Timestamp);
-                mDatabase.child(Timestamp).setValue(ann);
-                /*
+                firebaseAuth= FirebaseAuth.getInstance();
+                mDatabase = FirebaseDatabase.getInstance().getReference("Announcements");
+
+                String Subject = subject.getText().toString().trim();
+                String Content = content.getText().toString().trim();
+                String timestamp =New.toString();
+                Ann ann = new Ann(Subject, Content, timestamp);
+                addNotification(Subject);
+                mDatabase.push().setValue(ann);
+
                 Intent goToAnnouncement;
                 goToAnnouncement = new Intent(AddAnnouncement.this, Announcements.class);
                 startActivity(goToAnnouncement);
-                */
+
+            }
+
+            private void addNotification(String Subject) {
+                NotificationCompat.Builder builder =
+                        new NotificationCompat.Builder(AddAnnouncement.this)
+                                .setSmallIcon(R.drawable.vesitlogo)
+                                .setContentTitle("New Announcement")
+                                .setContentText(Subject);
+
+                Intent notificationIntent = new Intent(AddAnnouncement.this, Announcements.class);
+                PendingIntent contentIntent = PendingIntent.getActivity(AddAnnouncement.this, 0, notificationIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(contentIntent);
+
+                // Add as notification
+                NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(0, builder.build());
             }
         });
+
     }
 }
